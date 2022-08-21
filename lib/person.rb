4e8bae7c012653fc06d9721ccf9dcceb0a08e17c
+ <+>
2022-08-21 08:03:06 +0300

    commit tree b425d6b04e7f6b30d234b5a34b68da1336a7f553

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+     #setter method
+     def name=(name)
+      @name = 'Beyonce'
+      end
+    
+      # getter method
+      def name
+        @name
+      end
+
+       #setter method
+    def job=(job)
+        @job = 'Singer'
+      end
+    
+      # getter method
+      def job
+        @job
+      end
+
 end
